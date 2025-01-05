8765b8b796e7bba881fd826a94fa3a12777584da
rainx0r <angelos.h@hotmail.com>
2025-01-05 17:17:43 +0200

    Add test stubs

---
+++ cpp_python_interop.pyi
@@ -0,0 +1,9 @@
+from typing import Any
+
+def my_function(data: Any) -> None:
+    """Test docs for my_function
+
+    Args:
+        data(mujoco._structs.MjData): the mjData struct
+    """
+    ...
