f2ab99e2ebc52a3352e2292d72bcf9f80213ce91
rainx0r <angelos.h@hotmail.com>
2025-01-05 20:18:27 +0200

    Add lsp config and formatting

--- custom_module.cpp
+++ custom_module.cpp
@@ -1,12 +1,9 @@
-#include <mujoco/mujoco.h>
 #include <iostream>
+#include <mujoco/mujoco.h>
 
 using namespace std;
 
-void my_function(mjData* data) {
-    /*cout << data->qpos[0] << endl;*/
-    /*cout << data->qpos[2] << endl; */
-    /*cout << data->qpos[3] << endl;*/
-    double res = data->qpos[0] * 2.0;
-    cout << res << endl;
+void my_function(mjData *data) {
+  double res = data->qpos[0] * 2.0;
+  cout << res << endl;
 }
