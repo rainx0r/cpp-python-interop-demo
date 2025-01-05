7992685a92009d634d6e9b8a0f783b0d4018fe4e
rainx0r <angelos.h@hotmail.com>
2025-01-05 20:11:50 +0200

    C++ LSP configuration and formatting

--- custom_module_bindings.cpp
+++ custom_module_bindings.cpp
@@ -1,14 +1,16 @@
-#include <nanobind/nanobind.h>
-#include <mujoco/mujoco.h>
 #include "custom_module.cpp"
+#include <mujoco/mujoco.h>
+#include <nanobind/nanobind.h>
 
 namespace nb = nanobind;
 
 NB_MODULE(cpp_python_interop, m) {
-    m.def("my_function", [](nb::object d) {
+  m.def(
+      "my_function",
+      [](nb::object d) {
         std::uintptr_t ptr = nb::cast<std::uintptr_t>(d.attr("_address"));
-        /*std::uintptr_t d_raw = d.attr("_address").cast<std::uintptr_t>();*/
-        mjData* d_cpp_ = reinterpret_cast<mjData*>(ptr);
+        mjData *d_cpp_ = reinterpret_cast<mjData *>(ptr);
         my_function(d_cpp_);
-    }, "Process mjData");
+      },
+      "Process mjData");
 }
